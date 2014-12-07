abfdf424f7aea38d4856c8a533ca4f783c8723af
Ashton Wai <AsHToN@InDeXSky.local>
2014-12-07 05:30:03 -0500

    Added login and signup page animation

---
+++ js/signup.js
@@ -0,0 +1,7 @@
+var $signup = $("#signup");
+var $bg_cover = $("#bg-cover");
+
+$(document).ready(function() {
+  TweenMax.fromTo($signup, 1.5, {y: -150}, {y: 0, ease: Sine.easeInOut});
+  TweenMax.fromTo($bg_cover, 1, {opacity: 0}, {opacity: 1, ease: Sine.easeInOut});
+});
